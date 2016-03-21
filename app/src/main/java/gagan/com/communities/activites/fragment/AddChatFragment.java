844efa4529ee767a690b5c78aa4aaffe4f6233bf
Gagandeep Singh <gdeep52@gmail.com>
2016-03-21 17:18:42 +0530

    chat fragment added ..

---
+++ app/src/main/java/gagan/com/communities/activites/fragment/AddChatFragment.java
@@ -0,0 +1,152 @@
+package com.support.android.designlibdemo.chat;
+
+
+
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gagan.com.communities.R;
+import gagan.com.communities.adapters.FollowerFollowingAdapter;
+import gagan.com.communities.models.HomeModel;
+import gagan.com.communities.models.UserDataModel;
+import gagan.com.communities.utills.GlobalConstants;
+import gagan.com.communities.webserviceG.CallBackWebService;
+import gagan.com.communities.webserviceG.SuperWebServiceG;
+
+public class AddChatFragment extends BaseFragmentG
+{
+
+
+    public AddChatFragment()
+    {
+        // Required empty public constructor
+    }
+
+    RecyclerView recyclerList;
+    ProgressBar progressBar;
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    )
+    {
+        View v = inflater.inflate(R.layout.recycler_list_layout, container, false);
+
+
+        progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        progressBar.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.button_pink), PorterDuff.Mode.MULTIPLY);
+        recyclerList = (RecyclerView) v.findViewById(R.id.recyclerList);
+        recyclerList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        hitWebserviceG();
+
+//        FollowerFollowingAdapter msgAdapter = new FollowerFollowingAdapter(getActivity(), null);
+//        recyclerList.setAdapter(msgAdapter);
+
+        return v;
+    }
+
+
+    void hitWebserviceG()
+    {
+        try
+        {
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            JSONObject data = new JSONObject();
+            data.put("user_id", "1");
+
+//            data.put("user_id", "1");
+
+            data.put("f_status", getArguments().getString("follower"));  // 1 for followers 2 for following.
+
+
+            new SuperWebServiceG(GlobalConstants.URL + "getfollowerList", data, new CallBackWebService()
+            {
+                @Override
+                public void webOnFinish(String output)
+                {
+                    progressBar.setVisibility(View.GONE);
+                    processOutput(output);
+                }
+            }).execute();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void processOutput(String output)
+    {
+        try
+        {
+
+            JSONObject jsonMain = new JSONObject(output);
+
+            JSONObject jsonMainResult = jsonMain.getJSONObject("result");
+
+            if (jsonMainResult.getString("code").contains("200"))
+            {
+
+
+                JSONArray jsonarrayData;
+
+                    jsonarrayData= jsonMainResult.getJSONArray("following");
+
+
+
+//                {"uId":"5","name":"Gagan","email":"gagan2@gmail.com","password":"e10adc3949ba59abbe56e057f20f883e","gender":"male","home_society":"bdbdbdbd","session_key":"","create_date":"2016-02-25 18:02:51","role_id":"2","profession":"sharp shooter","location":"Jagadhari Road, Sarsehri, Haryana 133004, India","delete_status":"0","device_type":"0","device_token":"0","update_date":"2016-02-25 11:02:51","profile_pic":"","path":"","is_fb":"0","facebook_id":"","is_gp":"0","gplus_id":"","login_status":"0"},
+
+                List<UserDataModel> list = new ArrayList<>();
+
+                for (int g = 0; g < jsonarrayData.length(); g++)
+                {
+                    JSONObject jobj = jsonarrayData.optJSONObject(g);
+
+                    UserDataModel homemodel = new UserDataModel();
+
+                    homemodel.setProfile_pic(jobj.optString("profile_pic"));
+                    homemodel.setuId(jobj.optString("uId"));
+                    homemodel.setName(jobj.optString("name"));
+                    homemodel.setProfession(jobj.optString("profession"));
+
+
+                    list.add(homemodel);
+                }
+
+                FollowerFollowingAdapter msgAdapter = new FollowerFollowingAdapter(getActivity(), list);
+                recyclerList.setAdapter(msgAdapter);
+
+            }
+            else
+            {
+//                Utills.showToast("No users available", getActivity(), true);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+}
